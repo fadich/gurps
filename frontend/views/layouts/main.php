aeb01cec5cb716adb09eef3589bd54ee81756214
Daniel <email>
2016-08-13 13:35:28 +0300

    Users-table

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
+<body onload="search()">
 <?php $this->beginBody() ?>
 
 <div class="wrap">
